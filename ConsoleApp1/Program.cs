faf528162654817421d056405197eabe14da7b62
freute <54909628+freute@users.noreply.github.com>
2024-10-02 22:04:11 +0000

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using  libt;
+
+    
+
+ Man m=new Man("hey") ;
+ Console.WriteLine("Hello, World! " + new Man("okk"));  
+
+ void doo(){
+   Console.WriteLine("Hello, World!" + new Man("okk"));   
+}
+
+doo();
+
+// See https://aka.ms/new-console-template for more information
+
---
+++ ConsoleApp1/libt/test1.cs
@@ -0,0 +1,16 @@
+namespace libt{
+
+
+public enum Myenum{
+    value1,
+    field,
+
+}
+public class Man{
+string name;
+public Man(string v){
+
+    name=v;
+}    
+}
+}
